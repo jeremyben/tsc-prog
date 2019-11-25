b3d548a9cf4e56220615033137aff2613f4c4465
jeremyben <bensimon.jeremy@gmail.com>
2019-11-25 22:06:08 +0100

    test: bundle duplicate declarations

---
+++ __fixtures__/bundle/duplicate/main.ts
@@ -0,0 +1,10 @@
+/** X */
+export class X {}
+export { X as Y }
+
+/** A */
+class A {}
+export { A as B }
+export { A as C }
+
+export default A
--- __tests__/bundle-addon.ts
+++ __tests__/bundle-addon.ts
@@ -1,19 +1,49 @@
+import { readFileSync } from 'fs'
 import { join } from 'path'
-import { build } from '../src'
+import { build, TsConfigCompilerOptions } from '../src'
 
-const basePath = join(__dirname, '..', '__fixtures__', 'bundle-complex')
+const fixture = (...path: string[]) => join(__dirname, '..', '__fixtures__', 'bundle', ...path)
 
-test.skip('bundle', () => {
+const compilerOptions: TsConfigCompilerOptions = { rootDir: '.', outDir: 'dist' }
+
+test('duplicate', () => {
+	const entryPoint = fixture('duplicate', 'dist', 'main.d.ts')
+
 	build({
-		basePath,
-		configFilePath: 'tsconfig.json',
-		compilerOptions: {
-			declaration: true,
-			listEmittedFiles: true,
-		},
-		exclude: ['**/excluded'],
+		basePath: fixture('duplicate'),
+		extends: '../tsconfig.json',
+		compilerOptions,
+		clean: { outDir: true },
 		bundleDeclaration: {
-			entryPoint: 'main.d.ts',
+			entryPoint,
 		},
 	})
+
+	const bundled = readFileSync(entryPoint, 'utf8')
+
+	for (const expected of [
+		'export declare class X',
+		'export { X as Y }',
+		'export declare class B',
+		'export declare class C',
+		'export default B',
+	]) {
+		expect(bundled).toContain(expected)
+	}
+})
+
+test.skip('complex', () => {
+	const entryPoint = fixture('complex', 'dist', 'main.d.ts')
+
+	build({
+		basePath: fixture('complex'),
+		extends: '../tsconfig.json',
+		compilerOptions,
+		bundleDeclaration: {
+			entryPoint,
+		},
+	})
+
+	const bundled = readFileSync(entryPoint, 'utf8')
+	console.log(bundled)
 })
